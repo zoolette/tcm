e36755dd31ab879faf7b0c1069c6d7309bfd9b4c
zoolette <gaufrette@gmail.com>
2023-09-28 19:08:53 +0200

    Try css custom file injection

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -1,3 +1,9 @@
+const $head = document.querySelector('head');
+const customStyle = '<link href="https://cdn.jsdelivr.net/gh/zoolette/tcm@latest/style/custom.css" rel="stylesheet" type="text/css" />';
+if ($head) {
+	$head.insertAdjacentHTML('beforeend', customStyle);
+}
+
 const burgerMenu = '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar-collapse" aria-expanded="false">' +
 	                   '<span class="visible-xs h-font">Menu</span>' +
 	                   '<span class="burger">' +
@@ -7,7 +13,6 @@ const burgerMenu = '<button type="button" class="navbar-toggle collapsed" data-t
 	                   '</span>' + 
 	                   '</button>';
 const $socialLinks = document.querySelector('ul.social');
-console.log($socialLinks);
 if ($socialLinks) {
 	    $socialLinks.insertAdjacentHTML('afterbegin', burgerMenu );
 }
